3305e8143c8c9bd28fe96fb8bd531cbd2535d08b
Ronaldo <Ronaldo@Ronaldo-PC>
2017-09-21 19:42:19 -0300

    Inicio do projeto

---
+++ src/trabalho01_joseronaldosilveira/Trabalho01_JoseRonaldoSilveira.java
@@ -0,0 +1,13 @@
+/*
+ * Criado por José Ronaldo Silveira Miguel
+ * V01 - 21/09/2017
+ */
+package trabalho01_joseronaldosilveira;
+
+
+public class Trabalho01_JoseRonaldoSilveira {
+
+    public static void main(String[] args) {
+    }
+    
+}
